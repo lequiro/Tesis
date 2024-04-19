6ebb22285ed17d65ad5984e3f384b1c4a983cebe
lequiro <103467156+lequiro@users.noreply.github.com>
2024-04-19 09:07:47 -0300

    training data y ae

--- Script/ks_reconstruido.py
+++ Script/ks_reconstruido.py
@@ -54,8 +54,8 @@ def solve_KS(initial_condition, spatial_resolution, time_steps, order=4):
 
 #%%
 L = 22 # Longitud del dominio
-T = 100  # Tiempo total de simulación
-nx = 50  # Número de puntos de la cuadrícula
+T = 60  # Tiempo total de simulación
+nx = 64 # Número de puntos de la cuadrícula
 dt = 1e-3
 nt = int(T/dt)  # Número de pasos de tiempo
 
@@ -68,17 +68,20 @@ k = 2*np.pi*np.fft.rfftfreq(nx, dx) #te da las frecuencias en radianes dado un n
 #Condiciones Iniciales: deben ser L-periódicas
 p  = 2*np.pi/L
 # u0 = np.sin(p*x) + 0.5*np.sin(2*p*x) + 0.1*np.sin(3*p*x - L/3)
-u0 = np.cos((2 * np.pi * x) / L) + 0.1 * np.cos((4 * np.pi * x) / L)
+u0 = np.cos(p* x) + 0.1 * np.cos(2*p* x)
 
 
 u, u_hat = solve_KS(u0, nx, nt)
+
+#corto el transitorio
+u = u[:,20_000:]
 #%%
 
 # Create subplots
 fig, axs = plt.subplots(2, 2, figsize=(12, 10))
 
 # Plot first graph
-axs[0, 0].imshow(u, extent=[0, T, 0, L], aspect='auto')
+axs[0, 0].imshow(u, extent=[20, T, 0, L], aspect='auto')
 axs[0, 0].set_title('u')
 axs[0, 0].set_xlabel('Tiempo')
 axs[0, 0].set_ylabel('Espacio')
@@ -93,17 +96,27 @@ axs[0, 1].set_title('Densidad de energía')
 # Plot third graph
 seleccion_x = np.sort(np.random.choice(nx, size=20, replace=False))
 for i in seleccion_x:
-    axs[1, 0].plot(t, u[i, :])
+    axs[1, 0].plot(t[20_000:], u[i, :])
 axs[1, 0].set_xlabel('Tiempo')
 axs[1, 0].set_ylabel('Amplitud')
 axs[1, 0].set_title('20 selecciones aleatorias de u')
 
 # Plot fourth graph
 real_space = np.sum(np.square(u), axis=0)
-axs[1, 1].plot(t, real_space)
+axs[1, 1].plot(t[20_000:], real_space)
 axs[1, 1].set_xlabel('Tiempo')
 axs[1, 1].set_ylabel('Energía total $\sum_{columnas} u^2$')
 axs[1, 1].set_title('Energía total')
 
 plt.tight_layout()
 plt.show()
+#%%
+plt.close('all')
+plt.loglog( k, np.abs(u_hat[:,2]), label= 't=2e-3')
+plt.loglog( k, np.abs(u_hat[:,1_000]), label= 't=1')
+plt.loglog( k, np.abs(u_hat[:,2_000]), label= 't=2')
+plt.loglog( k, np.abs(u_hat[:,5_000]), label= 't=5')
+plt.loglog( k, np.abs(u_hat[:,10_000]), label= 't=10')
+# plt.loglog( k, np.abs(u_hat[:,20_000]), label= 't=20')
+# plt.loglog( k, np.abs(u_hat[:,40_000]), label= 't=40')
+plt.legend()
