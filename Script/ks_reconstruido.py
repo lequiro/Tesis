ef9632db2b9b9a64a11380ea19dec0e76969a82c
lequiro <103467156+lequiro@users.noreply.github.com>
2024-05-24 09:57:33 -0300

    graficos y nuevo script

--- Script/ks_reconstruido.py
+++ Script/ks_reconstruido.py
@@ -1,9 +1,11 @@
 import numpy as np
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-from IPython import get_ipython
-import random
-get_ipython().run_line_magic('matplotlib', 'qt5')
+import os
+#directorio donde guardo los datasets
+corrida=1
+dh=10
+path = rf'C:\Users\Luis Quispe\Desktop\Tesis\data_KS\Datasets\dh_{dh}\{corrida}'
+os.chdir(path)
 
 #%%
 def solve_KS(initial_condition, spatial_resolution, time_steps, order=4):
@@ -57,112 +59,41 @@ def solve_KS(initial_condition, spatial_resolution, time_steps, order=4):
 def generar_funcion_periodica_aleatoria(num_terminos):
     funcion = np.zeros_like(x)
     for i in range(num_terminos):
-        frecuencia = np.random.randint(1, 3)  # Frecuencia aleatoria
+        frecuencia = np.random.randint(1, 4)  # Frecuencia aleatoria
         amplitud = np.random.uniform(0.1, 0.95)  # Amplitud aleatoria
         # fase = np.random.uniform(0, 2*np.pi)  # Fase aleatoria
-        funcion += amplitud * np.cos(frecuencia * p * x)
+        funcion = funcion +  amplitud * np.cos(frecuencia * p * x)
     return funcion
 
 #%%
 L = 22 # Longitud del dominio
-T = 60  # Tiempo total de simulación
-nx = 80 # Número de puntos de la cuadrícula
+T = 400 # Tiempo total de simulación
+nx = 64 # Número de puntos de la cuadrícula
 dt = 1e-3
 nt = int(T/dt)  # Número de pasos de tiempo
+corte_transitorio = 40_000
 
-x, dx = np.linspace(0, L, nx, endpoint=False, retstep=True)
-t, dt = np.linspace(0, T, nt, endpoint=False, retstep=True)
+x, dx = np.linspace(0, L, nx, endpoint=True, retstep=True)
+t, dt = np.linspace(0, T, nt, endpoint=True, retstep=True)
 k = 2*np.pi*np.fft.rfftfreq(nx, dx) #te da las frecuencias en radianes dado un número de puntos y un espaciado
 p  = 2*np.pi/L
 
+#Creo el data set de entrenamiento
+u0 = 0.3*np.cos(3*p*x) + 0.4*np.cos(2*p*x) + .5*np.cos(p*x)
+u_train, u_hat_train = solve_KS(u0, nx, nt)
+u_train = u_train[:,corte_transitorio:]
 
-num_it = 10
-# Crear un tensor para almacenar las matrices u
-u_total = np.ones((nx, nt, num_it))
-u_hat_total = np.ones((nx//2 + 1, nt, num_it), dtype=complex)
 
+#Creo el data set de test
 
-for i in range(num_it):
-    # Generar una función periódica aleatoria
-    u0 = generar_funcion_periodica_aleatoria(random.randint(1, 3))
-    
-    # Resolver la ecuación de Kuramoto-Sivashinsky
-    u, u_hat = solve_KS(u0, nx, nt)
-    
-    # Almacenar el resultado en el tensor u_total
-    u_total[:, :, i] = u
-    u_hat_total[:,:,i] = u_hat
-
-# corto el transitorio
-u_total = u_total[:, 20_000:, :]
-u_hat_total = u_hat_total[:, 20_000:, :]
-#%%
-#la primera vez para ver cuales quitar
-u_hat_total_new = u_hat_total.copy()
-u_total_new = u_total.copy()
-#%%
-indices_to_delete = [0,5,6]
-u_total_new = np.delete(u_total, indices_to_delete, axis=2)
-u_hat_total_new = np.delete(u_hat_total, indices_to_delete, axis=2)
-#%%
-# u_total_new = np.load("u_total.npy")
-# u_hat_total_new = np.load("u_hat_total.npy")
-# Create subplots
-fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-
-# Plot first graph
-axs[0, 0].imshow(u, extent=[20, T, 0, L], aspect='auto')
-axs[0, 0].set_title('u')
-axs[0, 0].set_xlabel('Tiempo')
-axs[0, 0].set_ylabel('Espacio')
-fig.colorbar(axs[0, 0].imshow(u_total_new[:,:,1], extent=[20, T, 0, L], aspect='auto'), ax=axs[0, 0])
-
-# Plot second graph
-for r in range(10 - len(indices_to_delete)):
-    axs[0, 1].loglog(k, np.abs(u_hat_total_new[:,-1,r]), label= f'{r}')
-axs[0, 1].set_xlabel('k')
-axs[0, 1].set_ylabel('Densidad de energía $|u(k)|$')
-axs[0, 1].set_title('Densidad de energía')
-axs[0, 1].legend()
-
-# Plot third graph
-seleccion_x = np.sort(np.random.choice(nx, size=20, replace=False))
-for i in seleccion_x:
-    axs[1, 0].plot(t, u[i, :])
-axs[1, 0].set_xlabel('Tiempo')
-axs[1, 0].set_ylabel('Amplitud')
-axs[1, 0].set_title('20 selecciones aleatorias de u')
-
-# Plot fourth graph
-for r in range(10 - len(indices_to_delete)):
-    real_space = np.sum(np.square(u_total_new[:,:,r]), axis=0)
-    axs[1, 1].plot(t[20_000:], real_space)
-axs[1, 1].set_xlabel('Tiempo')
-axs[1, 1].set_ylabel('Energía total $\sum_{columnas} u^2$')
-axs[1, 1].set_title('Energía total')
-
-plt.tight_layout()
-plt.show()
+u0 = 0.3*np.cos(p*x) + 0.4*np.cos(2*p*x) + .5*np.cos(5*p*x) + 0.2*np.cos(p*x + 0.5*np.pi)
+u_test, u_hat_test = solve_KS(u0, nx, nt)
+u_test = u_test[:,corte_transitorio:]
 
 #%%
-import os
-'''
-Si todo anda bien guardo el tensor que tiene los datos
-'''
-mes = '04'
-path = rf'C:\Users\Luis Quispe\Desktop\Tesis\data_KS\{mes}_24'
-os.chdir(path)
-
-np.save("u_total_1.npy", u_total_new)
-np.save("u_hat_total_1.npy", u_hat_total_new)
-
-#%%
-plt.close('all')
-plt.loglog( k, np.abs(u_hat[:,2]), label= 't=2e-3')
-plt.loglog( k, np.abs(u_hat[:,1_000]), label= 't=1')
-plt.loglog( k, np.abs(u_hat[:,2_000]), label= 't=2')
-plt.loglog( k, np.abs(u_hat[:,5_000]), label= 't=5')
-plt.loglog( k, np.abs(u_hat[:,10_000]), label= 't=10')
-# plt.loglog( k, np.abs(u_hat[:,20_000]), label= 't=20')
-# plt.loglog( k, np.abs(u_hat[:,40_000]), label= 't=40')
-plt.legend()
+#guardo los datasets
+np.save(r'variables\u_train.npy', u_train)
+np.save(r'variables\u_hat_train.npy', u_hat_train[:,corte_transitorio:])
+np.save(r'variables\u_test.npy', u_test)
+np.save(r'variables\u_hat_test.npy', u_hat_test[:,corte_transitorio:])
+np.savez(r'variables\data.npz', L=L, T=T, nx=nx, t=t[corte_transitorio:], k=k)
--- ae_test/03/cnnae.py
+++ ae_test/03/cnnae.py
@@ -1,14 +1,25 @@
 import numpy as np
+import pandas as pd
+import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
 from tensorflow.keras.layers import Input, Dense, Conv1D, MaxPooling1D, UpSampling1D, Reshape
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import CSVLogger
-
-def gaussian(x, x0):
-    return np.exp(-1000*np.abs(x-x0)**2)
-
+from IPython import get_ipython
+corrida=1
+dh=32
+path = rf'C:\Users\Luis Quispe\Desktop\Tesis\data_KS\Datasets\dh_{dh}\{corrida}\variables'
+os.chdir(path)
+get_ipython().run_line_magic('matplotlib', 'qt5')
+#%%
+u_train = np.load('u_train.npy')
+u_hat_train = np.load('u_hat_train.npy')
+u_test = np.load('u_test.npy')
+u_hat_test = np.load('u_hat_test.npy')
+#%%
 # Custom loss function
 @tf.function
 def grad_loss(y_true, y_pred):
@@ -17,15 +28,13 @@ def grad_loss(y_true, y_pred):
     dy_pred = tf.experimental.numpy.diff(y_pred, axis=1)*dx
     loss_deriv = tf.keras.losses.mse(dy_true, dy_pred)
     return loss_deriv
-
-N = 200
-x_dom = np.linspace(0, 1, num=N)
-
-# plt.plot(x_dom, gaussian(x_dom, 0.5))
-# plt.show()
-xnots = np.random.rand(500)
-x_data = np.array([gaussian(x_dom, rr) for rr in xnots])
-x_valid = np.array([gaussian(x_dom, rr) for rr in np.linspace(0,1,num=10)])
+#%%
+N= (u_train.T).shape[1]
+num_batch_size = 128
+num_epochs = 500
+num_valid_split = 0.3
+x_data = u_train.T[::10,:]
+x_valid = u_test.T[::10,:]
 
 # Define the input shape
 input_shape = (N,1)
@@ -35,12 +44,12 @@ inputs = Input(shape=input_shape)
 x = Conv1D(16, 3, activation='relu', padding='same')(inputs)
 x = Conv1D(8, 3, activation='relu', padding='same')(x)
 x = Conv1D(8, 3, activation='relu', padding='same')(x)
-x = Reshape((8*200,))(x)
-encoded = Dense(1)(x)
+x = Reshape((8*N,))(x)
+encoded = Dense(dh)(x)
 
 # Decoder
-x = Dense(8*200, activation='relu')(encoded)
-x = Reshape((200, 8))(x)
+x = Dense(8*N, activation='relu')(encoded)
+x = Reshape((N, 8))(x)
 x = Conv1D(8, 3, activation='relu', padding='same')(x)
 x = Conv1D(8, 3, activation='relu', padding='same')(x)
 x = Conv1D(16, 3, activation='relu', padding='same')(x)
@@ -48,28 +57,73 @@ decoded = Conv1D(1, 3, padding='same')(x)
 
 # Autoencoder model
 autoencoder = Model(inputs, decoded)
-optimizer = Adam(learning_rate=1e-3)
+optimizer = Adam(learning_rate=1e-4)
 autoencoder.compile(optimizer=optimizer,
                     loss='mse',
                     metrics=['MeanSquaredError', grad_loss])
 
 # Callback
-csv_logger = CSVLogger('output.csv', append=True)
+csv_logger = CSVLogger(f'..\output_cnnae_{num_batch_size}_{num_epochs}.csv', append=True)
 
 # Train
-for ii in range(30):
-    history = autoencoder.fit(x_data, x_data,
-                              epochs=1000,
-                              batch_size=32,
-                              validation_split=0.2,
-                              callbacks=[csv_logger],
-                              verbose=0)
+
+history = autoencoder.fit(x_data, x_data,
+                          epochs=num_epochs,
+                          batch_size=num_batch_size,
+                          validation_split=num_valid_split,
+                          callbacks=[csv_logger],
+                          verbose=0)
 
 
-    # Save output
-    preds = autoencoder(x_valid).numpy()
-    np.save(f'preds{ii:02}', preds[1, :])
+# Save output
+preds = autoencoder(x_valid).numpy()[:,:,0]
+#%%
+df_cnnae = pd.read_csv(f'..\output_cnnae_{num_batch_size}_{num_epochs}.csv')
+#%%%
+num = 10_000
 
-    # Change learning rate
-    if ii==10:
-        optimizer.learning_rate.assign(1e-4)
+plt.figure(figsize=(10, 6))
+plt.plot(preds.T[:, num], label='predicción', linewidth=2)
+plt.plot(x_valid.T[:, num], label='data KS', linewidth=2)
+plt.title('Convolutional Neural Network', fontsize=16)
+plt.xlabel('x', fontsize=14)
+plt.ylabel('u(x)', fontsize=14)
+plt.legend(fontsize=12)
+
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+plt.savefig(f'..\cnnae_{num}.png', dpi=300, bbox_inches='tight')
+plt.show()
+#%%
+#LOSS FUNCTION
+plt.figure(figsize=(10, 6))
+plt.plot(df_cnnae['MeanSquaredError'], label='MSE_TRAIN', linewidth=2)
+plt.plot(df_cnnae['val_MeanSquaredError'], label='MSE_VALID', linewidth=2)
+plt.title('Training and Validation Mean Squared Error CNNAE', fontsize=16)
+plt.xlabel('Epoch', fontsize=14)
+plt.ylabel('Mean Squared Error', fontsize=14)
+plt.legend(fontsize=12)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+
+plt.savefig('..\mse_plot_cnnae.png', dpi=300, bbox_inches='tight')
+plt.show()
+#%%
+# Difference between valid and train
+plt.figure(figsize=(10, 6))
+plt.semilogy(df_cnnae['epoch'], df_cnnae['val_MeanSquaredError'] - df_cnnae['MeanSquaredError'])
+
+plt.xlabel('Epoch', fontsize=14)
+plt.ylabel('Validation MSE - Training MSE', fontsize=14)
+plt.title('Difference between Validation and Training MSE over Epochs', fontsize=18)
+
+plt.xticks(fontsize=12)
+
+# Manually set the y-ticks to include only the desired ticks
+desired_yticks = [10**-2, 10**-1, 10**-0.5]
+plt.yticks(desired_yticks, fontsize=12)
+
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+plt.show()
+
